0c5ca15dbe8e3821014b7be44c7489aaec73d4f3
Jorrit Ligtvoet <jorrit.ligtvoet@synchronlab.com>
2021-08-24 13:07:41 +0200

    added denso sources

---
+++ Denso/Robot2 THS 001/Robot2 THS 001/Source Files/VAR_TAB.h
@@ -0,0 +1,62 @@
+'===== Type I Variables =====
+#define	ExternalSpeed	0	' General speed
+#define	Force_Decapping	1	' 
+#define	StateFinished	3	' 
+#define	TubeIndex	4	' 
+#define	RowCount	5	' 
+#define	RZ_For_Racks	7	' 
+#define	Rack_Height	8	' 
+#define	ProductHeight	9	' 
+#define	GoTo_Speed	10	' 
+#define	CarefulZ_Speed	11	' 
+#define	Initialize_Speed	12	' 
+#define	Cap_Speed	13	' 
+#define	MAX_Z_Height	14	' 
+'===== Type F Variables =====
+#define	Force_Decapping_LIVE	4	' Force_Decapping_LIVE
+#define	Force_Capping_LIVE	5	' Force_Capping_LIVE
+#define	HighSpeed_Force_Control	6	' Force_Control
+#define	LowSpeed_Force_Control	7	' 
+#define	GloPickFromRack	40	' 
+'===== Type D Variables =====
+'===== Type V Variables =====
+'===== Type P Variables =====
+#define	InitSafe_pos	0	' 
+#define	Pallet1_P1	1	' 
+#define	Pallet1_P2	2	' 
+#define	Pallet1_P3	3	' 
+#define	Pallet1_P4	4	' 
+#define	Pallet1_P11	5	' 
+#define	Pallet1_P12	6	' 
+#define	Pallet1_P13	7	' 
+#define	Pallet1_P14	8	' 
+#define	Pallet2_P21	9	' 
+#define	Pallet2_P22	10	' 
+#define	Pallet2_P23	11	' 
+#define	Pallet2_P24	12	' 
+#define	Pallet2_P31	13	' 
+#define	Pallet2_P32	14	' 
+#define	Pallet2_P33	15	' 
+#define	Pallet2_P34	16	' 
+#define	Pallet3_P41	17	' 
+#define	Pallet3_P42	18	' 
+#define	Pallet3_P43	19	' 
+#define	Pallet3_P44	20	' 
+#define	Pallet3_P51	21	' 
+#define	Pallet3_P52	22	' 
+#define	Pallet3_P53	23	' 
+#define	Pallet3_P54	24	' 
+#define	SafePosDecap	25	' 
+#define	Clamp_pos_Decap	26	' 
+#define	Clamp_pos_Cap	27	' 
+#define	WaitPos	28	' 
+#define	Middle_pos_2Rack	29	' 
+'===== Type J Variables =====
+'===== Type T Variables =====
+'===== Type S Variables =====
+#define	PumpLog	0	' 
+#define	SystemState	3	' 
+'===== Tool =====
+'===== Work =====
+'===== Area =====
+'===== Exclusive area =====
